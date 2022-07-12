679f1bfc44d70c914a81b9384c559eed331484cf
maryamhoss <seyedeh.maryam.hosseini.telgerdi@usherbrooke.ca>
2022-07-12 13:12:00 -0400

    code cleanup

--- main_conv_prediction.py
+++ main_conv_prediction.py
@@ -148,64 +148,14 @@ def main(exp_type, data_type,
     #                    train
     ##############################################################
 
-    print("fitting model")
+    print("training model")
 
-    path_model_plot = os.path.join(*[models_dir, 'model_plot.png'.format(exp_type)])
-    tf.keras.utils.plot_model(model, path_model_plot)
 
-    if 'voice_preprocessing' in data_type:
-        print('Voice Preprocessing:')
-        history_path = other_dir + '/log_voice_preprocessing.csv'
-        callbacks = get_callbacks(hours,path_best_model, history_path, data_type, images_dir)
-        generators = getData(sound_shape=(sound_len, 1),
-                             spike_shape=(sound_len, n_channels),
-                             sound_shape_test=(sound_len_test, 1),
-                             spike_shape_test=(sound_len_test, n_channels),
-                             data_type=data_type,
-                             batch_size=batch_size,
-                             downsample_sound_by=downsample_sound_by,
-                             test_type=test_type)
-
-        model.fit(generators['train'],
-                  epochs=epochs,
-                  validation_data=generators['val'],
-                  callbacks=callbacks)
-        data_type = data_type.replace('voice_preprocessing', '')
-
-        if epochs > 0:
-            history_df = pd.read_csv(history_path)
-            history_dict = {k: history_df[k].tolist() for k in history_df.columns.tolist()}
-            history = lambda x: None
-            history.history = history_dict
-            plot_filename = os.path.join(*[images_dir, 'history_voice_preprocessing.png'])
-            plot_history(history, plot_filename, epochs)
-            json_filename = os.path.join(*[other_dir, 'history_voice_preprocessing.json'])
-            history_jsonable = {k: np.array(v).astype(float).tolist() for k, v in history.history.items()}
-            json.dump(history_jsonable, open(json_filename, "w"))
 
     history_path = other_dir + '/log.csv'
     callbacks = get_callbacks(hours,path_best_model, history_path, data_type, images_dir)
-    print('Real Training')
-    if 'batch_increase' in data_type:
-        batches = range(batch_start, batch_stop, batch_step)
-        epochs = int(epochs / len(batches))
-        for b in batches:
-            print('batch equals:' + str(b))
-            generators = getData(sound_shape=(sound_len, 1),
-                                 spike_shape=(spike_len, n_channels),
-                                 sound_shape_test=(sound_len_test, 1),
-                                 spike_shape_test=(spike_len_test, n_channels),
-                                 data_type=data_type,
-                                 batch_size=b,
-                                 downsample_sound_by=downsample_sound_by,
-                                 test_type=test_type)
 
-            model.fit(generators['train'],
-                      epochs=epochs,
-                      validation_data=generators['val'],
-                      callbacks=callbacks)
-            
-    elif 'mes' in data_type:
+    if 'mes' in data_type:
         
         generators = getData_mes(sound_shape=(sound_len, 1),
                      spike_shape=(spike_len, n_channels),
@@ -253,16 +203,9 @@ def main(exp_type, data_type,
                   validation_data=generators['val'],
                   callbacks=callbacks)
 
-    print('fitting done, saving model')
+    print('training done, saving model')
 
-    if "noiseinput" in data_type:
-        model.save_weights(path_best_model)
-        symbolic_weights = getattr(model.optimizer, 'weights')
-        weight_values = K.batch_get_value(symbolic_weights)
-        with open(path_best_optimizer, 'wb') as f:
-            pickle.dump(weight_values, f)
-    else:
-        model.save(path_best_model)
+    model.save(path_best_model)
 
     if epochs > 0:
         history_df = pd.read_csv(history_path)
@@ -290,108 +233,15 @@ def main(exp_type, data_type,
     del callbacks
     del generators['train'], generators['test']
 
-    ##############################################################
-    #                    tests training
-    ##############################################################
-
     shutil.copyfile(FILENAME, text_dir + '/' + os.path.split(FILENAME)[-1])
     shutil.copyfile(os.path.join(CDIR, 'neural_models', 'models_convolutional.py'),
                     text_dir + '/' + 'models_convolutional.py')
-    shutil.copyfile(os.path.join(CDIR, 'neural_models', 'layers_transformer.py'),
-                    text_dir + '/' + 'layers_transformer.py')
-    shutil.copyfile(os.path.join(CDIR, 'neural_models', 'models_transformer_classic.py'),
-                    text_dir + '/' + 'models_transformer_classic.py')
     shutil.copyfile(os.path.join(CDIR, 'neural_models', '__init__.py'),
                     text_dir + '/' + '__init__.py')
 
-    results = {}
-    ends_at, ends_at_s = timeStructured(False, True)
-    results['starts_at'] = starts_at
-    results['ends_at'] = ends_at
-
-    duration_experiment = timedelta(seconds=ends_at_s - starts_at_s)
-    print(str(duration_experiment))
-    results['duration_experiment'] = str(duration_experiment)
-
-    if testing:
-        print('testing the model')
-
-        generators = getData(sound_shape=(sound_len, 1),
-                             spike_shape=(spike_len, n_channels),
-                             sound_shape_test=(sound_len_test, 1),
-                             spike_shape_test=(spike_len_test, n_channels),
-                             data_type=data_type,
-                             batch_size=1,
-                             downsample_sound_by=downsample_sound_by,
-                             test_type=test_type)
-        del generators['train'], generators['val']
-        prediction_metrics = ['stoi', ]  # , 'pesq']  'estoi', 'si-sdr'
-
-        noisy_metrics = [m + '_noisy' for m in prediction_metrics]
-        df1 = pd.DataFrame(columns=prediction_metrics + noisy_metrics)
-
-        prediction = []
-
-        inference_time = []
-        gt1 = generators['test']
-        gt2 = copy.deepcopy(generators['test'])
-        gt2.data_type = 'WithSpikes'
-        for batch_sample, (b1, b2) in enumerate(zip(gt1, gt2)):
-            noisy_snd = b2[0][0]
-            # eeg = b[0][1]
-            clean = b2[0][2]
-
-            intel_list = []
-            intel_list_noisy = []
-            print('batch sample is: ' + str(batch_sample))
-            print('sound length is: {}'.format(noisy_snd.shape[1]))
-            print('predicting')
-            inf_start_s = time.time()
-            pred = model.predict(b1[0])
-            inf_t = time.time() - inf_start_s
-            inference_time.append(inf_t)
-
-            fig_path = os.path.join(*[images_dir, 'prediction_{}.png'.format(batch_sample)])
-            one_plot_test(pred, clean, noisy_snd, exp_type, '', fig_path)
-
-            prediction.append(pred)
-            prediction_concat = np.concatenate(prediction, axis=0)
-            # print('saving sound')
-            # save_wav(pred, noisy_snd, clean, exp_type, batch_sample, fs, images_dir)
-
-            print('finding metrics')
-            for m in prediction_metrics:
-                print('     ', m)
-                pred_m = find_intel(clean, pred, metric=m)
-                intel_list.append(pred_m)
-
-                noisy_m = find_intel(clean, noisy_snd, metric=m)
-                intel_list_noisy.append(noisy_m)
-
-            e_series = pd.Series(intel_list + intel_list_noisy, index=df1.columns)
-            df1 = df1.append(e_series, ignore_index=True)
-
-        prediction_filename = os.path.join(*[images_dir, 'prediction_{}.npy'.format(exp_type)])
-        np.save(prediction_filename, prediction_concat)
-
-        del prediction, intel_list, intel_list_noisy, pred, prediction_concat, e_series
-        df1.to_csv(os.path.join(*[other_dir, 'evaluation.csv']), index=False)
-
-        import matplotlib.pyplot as plt
-
-        for column in df1.columns:
-            fig, ax = plt.subplots(1, figsize=(9, 4))
-
-            ax.set_title(column)
-            ax.violinplot(df1[column])
-            fig.savefig(os.path.join(*[images_dir, '{}.png'.format(column)]), bbox_inches='tight')
-
-        results['inference_time'] = np.mean(inference_time)
-    results_filename = os.path.join(*[other_dir, 'results.json'])
-    json.dump(results, open(results_filename, "w"))
 
     shutil.make_archive(ex.observers[0].basedir, 'zip', exp_dir)
 
-    return results
+
 
 
